4f9e2afa7ac4ce0b60a91b4e13c6f4122ce30c54
esyacelga <esyacelga@hotmail.com>
2023-11-05 14:01:53 -0500

    Logout

--- src/auth/context/auth-provider.jsx
+++ src/auth/context/auth-provider.jsx
@@ -6,17 +6,35 @@ import {types} from "../types/types.js";
 const initialState = {
     logged: false,
 }
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return {
+        logged: !!user,
+        user: user
+    }
+}
 export const AuthProvider = ({children}) => {
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, initialState, init);
+
+
     const login = (name = '') => {
+        const user = {
+            id: 'ABC',
+            name: name
+        }
         const action = {
             types: types.login,
-            payLoad: {
-                id: 'ABC',
-                name: name
-            }
+            payLoad: user
         }
+        localStorage.setItem('user', JSON.stringify(user))
         dispatch(action);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        const action = {}
+        dispatch(action);
+
     }
 
     return (
